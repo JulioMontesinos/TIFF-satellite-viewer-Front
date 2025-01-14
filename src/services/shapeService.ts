d0479dfeb5406a8cdefc71a7ce8fa72a4a06b77c
Julio <juliomontesinosalvarado@gmail.com>
2025-01-14 16:38:06 +0100

    Await in lower level for production

--- src/services/shapeService.ts
+++ src/services/shapeService.ts
@@ -1,11 +1,21 @@
 import axios from "axios";
 import { fetchToken } from "./apiService";
 
-const token = await fetchToken();
 const API_URL = import.meta.env.VITE_API_URL;
+
+// Encapsula el token en una función que se pueda llamar cuando sea necesario
+let token: string | null = null;
+
+const getToken = async (): Promise<string> => {
+  if (!token) {
+    token = await fetchToken(); // Solo llama a fetchToken una vez
+  }
+  return token;
+};
 
 export const checkShapesExist = async (): Promise<boolean> => {
   try {
+    const token = await getToken(); // Obtiene el token dinámicamente
     const response = await axios.get(`${API_URL}/count`, {
       headers: {
         Authorization: `Bearer ${token}`,
